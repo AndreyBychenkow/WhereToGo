737a8c1abad3af66b63c16dcaad1cac2ecd96815
AndreyBychenkow <decebell032@yandex.ru>
2025-01-24 13:29:04 +0300

    Create places app, Add Location model

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+	title = models.CharField(max_length=255)
+	place_id = models.CharField(max_length=100, unique=True)
+	details_url = models.URLField()
+
+	def __str__(self):
+		return self.title
