0213d0d6287266faef5459d2521b6a843c74d62d
AndreyBychenkow <decebell032@yandex.ru>
2025-01-25 20:39:52 +0300

    Added preview image

--- pages/admin.py
+++ pages/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
 from .models import Location, LocationImage
-
+from django.utils.safestring import mark_safe
 
 class LocationImageInline(admin.TabularInline):
     model = LocationImage
-    extra = 0
+    extra = 1
+
+    readonly_fields = ('image_preview',)
+
+    def image_preview(self, obj):
+        if obj.image:
+            return mark_safe(f'<img src="{obj.image.url}" style="max-height: 200px;"/>')
+        return "Нет изображения"
+
+    image_preview.short_description = 'Превью'
 
 
 @admin.register(Location)
